ca3a96f4abca7d2c87bed16d89374e34070e352f
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-07-23 06:40:55 +0200

    changed http to https to avoid mixed content

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayForecast(response) {
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/${
+          src="https://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
@@ -96,7 +96,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   sunriseElement.innerHTML = formatDate(response.data.sys.sunrise * 1000).slice(
